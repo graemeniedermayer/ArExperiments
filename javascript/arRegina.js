8c94df67e7f7076dbb0f2e64aef4fd19b9e00866
graemeniedermayer <38518120+graemeniedermayer@users.noreply.github.com>
2022-01-18 16:51:32 -0600

    Update and rename arRegina.js to javascript/arRegina.js

--- javascript/arRegina.js
+++ javascript/arRegina.js
@@ -56,7 +56,7 @@ function init() {
     directionalLight2.position.set( -0.9, -1, -0.2 ).normalize();
     scene.add( directionalLight2 );
 	var loader = new THREE.GLTFLoader()
-	loader.load( 'regina.gltf', function ( gltf ) {
+	loader.load( 'data/regina.gltf', function ( gltf ) {
 		gltf.scene.scale.z = 0.001
 		gltf.scene.scale.y = 0.001
 		gltf.scene.scale.x = 0.001
