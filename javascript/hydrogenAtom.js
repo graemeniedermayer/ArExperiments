59dbb94c0f3396a4a535a3c5eb0efc8a7ebf9078
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-03-22 13:11:29 -0600

    Update hydrogenAtom.js

--- javascript/hydrogenAtom.js
+++ javascript/hydrogenAtom.js
@@ -228,7 +228,7 @@ let onInput = ()=>{
 }
 if(window.Worker){
 	let lockWorker = false
-	strategyWorker = new Worker('/hydrogenWorker.js');
+	strategyWorker = new Worker('./hydrogenWorker.js');
     strategyWorker.postMessage({
 			'type':'grid',
 			'grid': grid,
