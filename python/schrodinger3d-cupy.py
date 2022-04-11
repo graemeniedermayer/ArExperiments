d069ab9a4381cd157191d100651aac153c6f4165
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-04-11 11:23:03 -0600

    Update schrodinger3d-cupy.py

--- python/schrodinger3d-cupy.py
+++ python/schrodinger3d-cupy.py
@@ -8,25 +8,3 @@ X,Y,Z= np.meshgrid(np.linspace(-L/2,L/2,N),np.linspace(-L/2,L/2,N),np.linspace(-
 V = cupyx.scipy.sparse.diags((0.0001*((Y)**2+(Z)**2+(X)**2)).reshape(N**3))
 eigenvalues, eigenvectors = sciSolve.eigsh(T+V,n)
 eprobs = cp.asnumpy(eigenvectors.T[n-1].reshape((N,N,N)))
-
-
-# this isn't the greatest..... assume arrays?
-def glassVal(glass,probs):
-    maxVal = L*(1/2-1/N)
-    minVal = -maxVal
-    glass[glass>maxVal] = maxVal
-    glass[glass<minVal] = minVal
-    indexs = (np.array(glass)/(L/N)) + N/2
-    bottoms = np.floor(indexs).astype(int)
-    tops = np.ceil(indexs).astype(int)
-    ratio = indexs-bottoms
-    ratioI = ratio - 1
-    v1 = ratio[:,2]*ratio[:,1]*ratio[:,0]*probs[bottoms[:,0],bottoms[:,1],bottoms[:,2]]
-    v2 = ratio[:,2]*ratio[:,1]*ratioI[:,0]*probs[bottoms[:,0],bottoms[:,1],tops[:,2]]
-    v3 = ratio[:,2]*ratioI[:,1]*ratioI[:,0]*probs[bottoms[:,0],tops[:,1],tops[:,2]]
-    v4 = ratioI[:,2]*ratioI[:,1]*ratioI[:,0]*probs[tops[:,0],tops[:,1],tops[:,2]]
-    v5 = ratioI[:,2]*ratio[:,1]*ratioI[:,0]*probs[tops[:,0],bottoms[:,1],tops[:,2]]
-    v6 = ratioI[:,2]*ratio[:,1]*ratio[:,0]*probs[tops[:,0],bottoms[:,1],bottoms[:,2]]
-    v7 = ratioI[:,2]*ratioI[:,1]*ratio[:,0]*probs[tops[:,0],tops[:,1],bottoms[:,2]]
-    v8 = ratio[:,2]*ratioI[:,1]*ratio[:,0]*probs[bottoms[:,0],tops[:,1],bottoms[:,2]]
-    return v1+v2+v3+v4+v5+v6+v7+v8
