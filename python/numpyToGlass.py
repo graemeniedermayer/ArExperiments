efa8e13a5f971a5ee0ebcd6ffb7a63d18fbfef47
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-04-11 11:27:15 -0600

    Update numpyToGlass.py

--- python/numpyToGlass.py
+++ python/numpyToGlass.py
@@ -10,12 +10,12 @@ def glassVal(glass,probs):
     tops = np.ceil(indexs).astype(int)
     ratio = indexs-bottoms
     ratioI = ratio - 1
-    v1 = ratio[:,2]*ratio[:,1]*ratio[:,0]*probs[bottoms[:,0],bottoms[:,1],bottoms[:,2]]
-    v2 = ratio[:,2]*ratio[:,1]*ratioI[:,0]*probs[bottoms[:,0],bottoms[:,1],tops[:,2]]
-    v3 = ratio[:,2]*ratioI[:,1]*ratioI[:,0]*probs[bottoms[:,0],tops[:,1],tops[:,2]]
-    v4 = ratioI[:,2]*ratioI[:,1]*ratioI[:,0]*probs[tops[:,0],tops[:,1],tops[:,2]]
-    v5 = ratioI[:,2]*ratio[:,1]*ratioI[:,0]*probs[tops[:,0],bottoms[:,1],tops[:,2]]
-    v6 = ratioI[:,2]*ratio[:,1]*ratio[:,0]*probs[tops[:,0],bottoms[:,1],bottoms[:,2]]
-    v7 = ratioI[:,2]*ratioI[:,1]*ratio[:,0]*probs[tops[:,0],tops[:,1],bottoms[:,2]]
-    v8 = ratio[:,2]*ratioI[:,1]*ratio[:,0]*probs[bottoms[:,0],tops[:,1],bottoms[:,2]]
+    v1 = ratio[:,:,:,0]*ratio[:,:,:,1]*ratio[:,:,:,2]*probs[bottoms[:,:,:,0],bottoms[:,:,:,1],bottoms[:,:,:,2]]
+    v2 = ratio[:,:,:,0]*ratio[:,:,:,1]*ratioI[:,:,:,2]*probs[bottoms[:,:,:,0],bottoms[:,:,:,1],tops[:,:,:,2]]
+    v3 = ratio[:,:,:,0]*ratioI[:,:,:,1]*ratioI[:,:,:,2]*probs[bottoms[:,:,:,0],tops[:,:,:,1],tops[:,:,:,2]]
+    v4 = ratioI[:,:,:,0]*ratioI[:,:,:,1]*ratioI[:,:,:,2]*probs[tops[:,:,:,0],tops[:,:,:,1],tops[:,:,:,2]]
+    v5 = ratioI[:,:,:,0]*ratio[:,:,:,1]*ratioI[:,:,:,2]*probs[tops[:,:,:,0],bottoms[:,:,:,1],tops[:,:,:,2]]
+    v6 = ratioI[:,:,:,0]*ratio[:,:,:,1]*ratio[:,:,:,2]*probs[tops[:,:,:,0],bottoms[:,:,:,1],bottoms[:,:,:,2]]
+    v7 = ratioI[:,:,:,0]*ratioI[:,:,:,1]*ratio[:,:,:,2]*probs[tops[:,:,:,0],tops[:,:,:,1],bottoms[:,:,:,2]]
+    v8 = ratio[:,:,:,0]*ratioI[:,:,:,1]*ratio[:,:,:,2]*probs[bottoms[:,:,:,0],tops[:,:,:,1],bottoms[:,:,:,2]]
     return v1+v2+v3+v4+v5+v6+v7+v8
