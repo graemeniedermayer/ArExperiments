c3f440f77ecd482d702dd0b3e1ce1cd320edc943
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-05-10 21:35:24 -0600

    Update cartesian.py

--- python/django/views/cartesian.py
+++ python/django/views/cartesian.py
@@ -9,20 +9,20 @@ import logging
 logger = logging.getLogger(__name__)
 N, L, n = 30, 8, 8
 convRate = 0.00107
+
 # Center of the image
 scenePosition = np.array([0.0,0.5,-.5]) 
 sceneSize = np.array([1.0,1.0,1.0])
 glass = np.array(json.load(open('miniGlass.json')))
-# diag = np.ones([N],dtype='c8')
-# diags = np.array([diag,-2*diag, diag],dtype='c8')
 
+#For math
 diag = np.ones([N])
 diags = np.array([diag,-2*diag, diag])
 D = scipy.sparse.spdiags(diags, np.array([-1,0,1]), N, N)      
 T = -0.5*(scipy.sparse.kronsum(scipy.sparse.kronsum(D,D),D))
 potentialCap = 0.01
 baseV = np.zeros((30,30,30))
-# baseV = np.pad(baseV, pad_width=1, mode='constant', constant_values=potentialCap)
+# baseV = np.pad(baseV, pad_width=1, mode='constant', constant_values=potentialCap) Border
 
 
 X,Y,Z= np.meshgrid(np.linspace(-L/2,L/2,N),np.linspace(-L/2,L/2,N),np.linspace(-L/2,L/2,N))
@@ -40,8 +40,8 @@ def applyQuaternion( v, q ):
     y = v[1]
     z = v[2] 
 
-    qx = q[0]
-    qy = q[1]
+    qx = q[1]
+    qy = q[0]
     qz = q[2] 
     qw = q[3] 
 
@@ -59,7 +59,31 @@ def applyQuaternion( v, q ):
     z = iz * qw + iw * - qz + ix * - qy - iy * - qx
 
     return np.array([x,y,z])
+def applyInverseQuaternion( v, q ):
 
+    x = v[0]
+    y = v[1]
+    z = v[2] 
+
+    qx = -q[1]
+    qy = -q[0]
+    qz = -q[2] 
+    qw = q[3] 
+
+    # calculate quat * vector
+
+    ix = qw * x + qy * z - qz * y
+    iy = qw * y + qz * x - qx * z
+    iz = qw * z + qx * y - qy * x
+    iw = - qx * x - qy * y - qz * z
+
+    # calculate result * inverse quat
+
+    x = ix * qw + iw * - qx + iy * - qz - iz * - qy
+    y = iy * qw + iw * - qy + iz * - qx - ix * - qz
+    z = iz * qw + iw * - qz + ix * - qy - iy * - qx
+
+    return np.array([x,y,z])
 
 # this isn't the greatest..... assume arrays?
 def glassVal(glass,probs):
@@ -118,24 +142,17 @@ def potentialSimple(camPos, camQuat, conv, width, height, image):
     # 
     whratio = width/height
     fov = 80
-    size = 0.125*2*np.tan( (2*np.pi/360)*(fov/(2* whratio)) )
+    size = 0.25*2*np.tan( (2*np.pi/360)*(fov/(2* whratio)) )
 
     # this is wrong.
     zDistance = -conv*(image[::2] + 255*image[1::2])
     xDistance, yDistance= np.meshgrid( np.linspace(-size/2, size/2, height), np.linspace(-whratio*size/2,  whratio*size/2, width))
     xDistance = xDistance.reshape( width* height)*zDistance 
     yDistance = yDistance.reshape( width* height)*zDistance 
-    # convert to the correct reference frame
-    # this is apply4matrix function with numpy arrays
-    # e = worldMatrix
-    # w = 1/( e[ 3 ] * xDistance + e[ 7 ] * yDistance + e[ 11 ] * zDistance + e[ 15 ])
-    # x = ( e[ 0 ] * xDistance + e[ 4 ] * yDistance + e[ 8 ] * zDistance+ e[ 12 ] ) * w
-    # y = ( e[ 1 ] * xDistance + e[ 5 ] * yDistance + e[ 9 ] * zDistance+ e[ 13 ] ) * w
-    # z = ( e[ 2 ] * xDistance + e[ 6 ] * yDistance + e[ 10 ] * zDistance + e[ 14 ] ) * w
 
     depthPoints = np.array([xDistance, yDistance, zDistance])
-    depthPoints = applyQuaternion(depthPoints, camQuat).T
-    depthPoints += camPos
+    depthPoints = applyInverseQuaternion(depthPoints, camQuat).T
+    depthPoints += np.array([camPos[1],camPos[0],camPos[2]])
     np.save('depth.npy', depthPoints)
     # cut for square
     depthPoints = depthPoints[ 
@@ -149,10 +166,44 @@ def potentialSimple(camPos, camQuat, conv, width, height, image):
     depthPoints[:,2] = np.floor( (depthPoints[:,2] - boundingSquare['minz'] )*N ) 
     depthPoints = np.floor(depthPoints).astype(int)
     # depth.floor?
-    V = np.copy(baseV)
+    V = np.zeros((30,30,30))
     V[ depthPoints[:,0], depthPoints[:,1], depthPoints[:,2]] = potentialCap
     
     return V.reshape(N**3)
+
+def potentialGlass(camPos, camQuat, conv, width, height, image):
+    # project forwards
+    # 
+    whratio = width/height
+    fov = 80
+    size = 0.25*2*np.tan( (2*np.pi/360)*(fov/(2* whratio)) )
+
+    # this is wrong.
+    zDistance = -conv*(image[::2] + 255*image[1::2])
+    xDistance, yDistance= np.meshgrid( np.linspace(-size/2, size/2, height), np.linspace(-whratio*size/2,  whratio*size/2, width))
+    xDistance = xDistance.reshape( width* height)*zDistance 
+    yDistance = yDistance.reshape( width* height)*zDistance 
+
+    depthPoints = np.array([xDistance, yDistance, zDistance])
+    depthPoints = applyInverseQuaternion(depthPoints, camQuat).T
+    depthPoints += np.array([camPos[1],camPos[0],camPos[2]])
+    np.save('depth.npy', depthPoints)
+    # cut for square
+    depthPoints = depthPoints[ 
+        (depthPoints[:,0] > boundingSquare['minx']) & (depthPoints[:,0] < boundingSquare['maxx']) & 
+        (depthPoints[:,1] > boundingSquare['miny']) & (depthPoints[:,1] < boundingSquare['maxy']) & 
+        (depthPoints[:,2] > boundingSquare['minz']) & (depthPoints[:,2] < boundingSquare['maxz'])
+    ]
+    np.save('depthCut.npy', depthPoints)
+    depthPoints[:,0] = np.floor( (depthPoints[:,0] - boundingSquare['minx'] )*N ) 
+    depthPoints[:,1] = np.floor( (depthPoints[:,1] - boundingSquare['miny'] )*N ) 
+    depthPoints[:,2] = np.floor( (depthPoints[:,2] - boundingSquare['minz'] )*N ) 
+    depthPoints = np.floor(depthPoints).astype(int)
+    # depth.floor?
+    V = np.zeros((30,30,30))
+    V[ depthPoints[:,0], depthPoints[:,1], depthPoints[:,2]] = potentialCap
+    
+    return V
 
 @csrf_exempt
 def test(request):
@@ -196,7 +247,7 @@ def returnPotential(request):
 def returnGlass(request):
     image = np.array(json.loads(request.POST.get("image")))
     pose = json.loads(request.POST.get("pose"))
-    worldMatrix, conversionRate, height, width = pose 
+    camPos, camQuat, conversionRate, height, width = pose 
     return HttpResponse(
-        json.dumps( glassVal(glass, potentialSimple(worldMatrix, conversionRate, width, height, image).reshape(N,N,N) ).tolist() )
+        json.dumps( glassVal(glass, potentialGlass(camPos, camQuat, conversionRate, width, height, image) ).tolist() )
     )
