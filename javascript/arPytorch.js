5c58c1ecdb5aa4b52c3665da80454c0d1629a50b
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-05-13 15:59:57 -0600

    Create arPytorch.js
    This will only work with a running server

---
+++ javascript/arPytorch.js
@@ -0,0 +1,301 @@
+let scene, uniforms, renderer, light, camera, camBinding, gl, texture1, shaderMaterial, scaleGeo, whratio, glBinding, dcamera, shaderProgram, mesh; 
+      // XR globals.
+      let xrButton = null;
+      let xrRefSpace = null;
+let captureNext = true
+
+function getXRSessionInit( mode, options) {
+  	if ( options && options.referenceSpaceType ) {
+  		renderer.xr.setReferenceSpaceType( options.referenceSpaceType );
+  	}
+  	var space = (options || {}).referenceSpaceType || 'local-floor';
+  	var sessionInit = (options && options.sessionInit) || {};
+  
+  	// Nothing to do for default features.
+  	if ( space == 'viewer' )
+  		return sessionInit;
+  	if ( space == 'local' && mode.startsWith('immersive' ) )
+  		return sessionInit;
+  
+  	// If the user already specified the space as an optional or required feature, don't do anything.
+  	if ( sessionInit.optionalFeatures && sessionInit.optionalFeatures.includes(space) )
+  		return sessionInit;
+  	if ( sessionInit.requiredFeatures && sessionInit.requiredFeatures.includes(space) )
+  		return sessionInit;
+  
+  	var newInit = Object.assign( {}, sessionInit );
+  	newInit.requiredFeatures = [ space ];
+  	if ( sessionInit.requiredFeatures ) {
+  		newInit.requiredFeatures = newInit.requiredFeatures.concat( sessionInit.requiredFeatures );
+  	}
+  	return newInit;
+   }
+function init(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.001, 10 );
+  // var particles = 20*10*150;
+  light = new THREE.PointLight( 0xffffff,1.5 );
+  light.decay = 1
+  light.distance = 30;
+  light.position.x = 0
+  light.position.y = 1.5
+  light.position.z = -1.2
+  scene.add(light)
+  var ambient = new THREE.AmbientLight( 0x222222 );
+  scene.add( ambient );
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.xr.enabled = true;
+  // renderer.gammaOutput = true;
+  document.body.appendChild( renderer.domElement );
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  const geometry 
+  whratio = window.innerWidth/ window.innerHeight 
+  scaleGeo = 2*Math.tan( 2*Math.PI*camera.fov/(2*360) )= new THREE.PlaneGeometry(scaleGeo,  scaleGeo*whratio, window.innerWidth, window.innerHeight );
+  mesh = new THREE.Mesh( geometry, new THREE.ShaderMaterial() );
+  mesh.material = new THREE.ShaderMaterial( { 
+	  uniforms : {
+		  // uSampler: { value: new THREE.DataTexture(texture1, dcamera.width, dcamera.height) },
+		  coordTrans: {value:{
+			  x:1/viewport.width,
+			  y:1/viewport.height
+		  }}
+	  },
+	  vertexShader:  document.getElementById( 'vertexShader' ).textContent,
+	  fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+  } )
+  scene.add(mesh)
+}
+upateFunction = (image, imageWidth, imageHeight)=>{
+	mesh = new THREE.Mesh( geometry, new THREE.ShaderMaterial() );
+	// size oe
+	mesh.material.uSampler.value =  new THREE.DataTexture(texture1, imageWidth, imageHeight)
+	mesh.material.needUpdate = true
+}
+
+updateServer = (pose, image) => fetch('https://computeServer.ca/pyTorch/',{
+        method: 'POST',
+        body: xwwwform({
+            'pose':pose,
+            'image':image
+        }),
+        mode:'cors',  
+        headers: {
+            'X-Requested-With':'XMLHttpRequest',
+			'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(x=>x.json())
+    .then(x=>{
+        let {spectra, imageWidth, imageHeight} = x
+		spec = spectra.map(x=>parseFloat(x))
+        updateFunction(spec)
+        captureNext = true
+
+    }).catch(x=>console.log(x))
+
+
+function AR(){
+	var currentSession = null;
+	function onSessionStarted( session ) {
+		session.addEventListener( 'end', onSessionEnded );
+		renderer.xr.setSession( session );
+		gl = renderer.getContext()
+		gl.makeXRCompatible().then(x=>{
+			// could lead to race condition
+			initCameraCaptureScene(gl)
+			glBinding = new XRWebGLBinding(session, gl);
+		})
+		button.style.display = 'none';
+		button.textContent = 'EXIT AR';
+		currentSession = session;
+		session.requestReferenceSpace('local').then((refSpace) => {
+          xrRefSpace = refSpace;
+          session.requestAnimationFrame(onXRFrame);
+        });
+	}
+	function onSessionEnded( /*event*/ ) {
+		currentSession.removeEventListener( 'end', onSessionEnded );
+		renderer.xr.setSession( null );
+		button.textContent = 'ENTER AR' ;
+		currentSession = null;
+	}
+	if ( currentSession === null ) {
+
+        let options = {
+          requiredFeatures: ['depth-sensing', 'dom-overlay', 'camera-access'],
+          domOverlay: { root: document.body },
+		  depthSensing: {
+		    usagePreference: [ "cpu-optimized"],
+		    dataFormatPreference: ["luminance-alpha"]
+		  }
+        };
+		var sessionInit = getXRSessionInit( 'immersive-ar', {
+			mode: 'immersive-ar',
+			referenceSpaceType: 'local', // 'local-floor'
+			sessionInit: options
+		});
+		navigator.xr.requestSession( 'immersive-ar', sessionInit ).then( onSessionStarted );
+	} else {
+		currentSession.end();
+	}
+	renderer.xr.addEventListener('sessionstart',
+		function(ev) {
+			console.log('sessionstart', ev);
+			document.body.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+			renderer.domElement.style.display = 'none';
+		});
+	renderer.xr.addEventListener('sessionend',
+		function(ev) {
+			console.log('sessionend', ev);
+			document.body.style.backgroundColor = '';
+			renderer.domElement.style.display = '';
+		});
+}
+function initCameraCaptureScene(gl) {
+    var vertices = [
+        -1.0, 1.0, 0.0
+    ];
+
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertCode =
+    'attribute vec3 coordinates;' +
+    'void main(void) {' +
+        'gl_Position = vec4(coordinates, 1.0);' +
+        'gl_PointSize = 1.0;'+
+    '}';
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vertCode);
+    gl.compileShader(vertShader);
+
+    // NOTE: we must explicitly use the camera texture in drawing,
+    // otherwise uSampler gets optimized away, and the
+    // camera texture gets destroyed before we could capture it.
+    var fragCode =
+    'uniform sampler2D uSamples;' +
+    'void main(void) {' +
+        'gl_FragColor = texture2D(uSamples, vec2(0,0));' +
+    '}';
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShader, fragCode);
+    gl.compileShader(fragShader);
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertShader);
+    gl.attachShader(shaderProgram, fragShader);
+    gl.linkProgram(shaderProgram);
+
+    aCoordLoc = gl.getAttribLocation(shaderProgram, "coordinates");
+    uSamplerLoc = gl.getUniformLocation(shaderProgram, "uSamples");
+	uSamplerLocs = gl.getUniformLocation(shaderProgram, "uSampler");
+
+    let glError = gl.getError();
+    if (glError!= gl.NO_ERROR) {
+        console.log("GL error: " + glError);
+    }
+}
+function drawCameraCaptureScene(gl, cameraTexture, width, height) {
+    const prevShaderId = gl.getParameter(gl.CURRENT_PROGRAM);
+
+    gl.useProgram(shaderProgram);
+
+    // Bind the geometry
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(aCoordLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aCoordLoc);
+
+    // Bind the texture to 
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, cameraTexture);
+    gl.uniform1i(uSamplerLoc, 1);
+
+    // Draw the single point
+    gl.drawArrays(gl.POINTS, 0, 1);
+	
+    const prev_framebuffer = gl.getParameter(gl.FRAMEBUFFER_BINDING); // save the screen framebuffer ID
+
+    // Create a framebuffer backed by the texture
+    const framebuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, cameraTexture, 0);
+
+    // Read the contents of the framebuffer
+    const data = new Uint8Array(width * height * 4);
+    gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, data);
+    gl.deleteFramebuffer(framebuffer);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, prev_framebuffer); // bind back the screen framebuffer
+
+	texture1 = gl.createTexture();
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, texture1);
+	gl.uniform1i(uSamplerLocs, 0);
+	const level = 1;
+	const internalFormat = gl.RGBA;
+	const border = 0;
+	const srcFormat = gl.RGBA;
+	const srcType = gl.UNSIGNED_BYTE;
+	const pixel = data;  
+	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+				  width, height, border, srcFormat, srcType,
+				  pixel);
+
+	gl.useProgram(prevShaderId);
+	// returns a Uint8Array
+	return data 
+}
+count = 0
+function onXRFrame(t, frame) {
+    const session = frame.session;
+    session.requestAnimationFrame(onXRFrame);
+    const baseLayer = session.renderState.baseLayer;
+    const pose = frame.getViewerPose(xrRefSpace);
+	render()
+	light.position.z = -3 - 2*Math.sin(count)
+	count += 0.02
+	if (pose) {
+		for (const view of pose.views) {
+            const viewport = baseLayer.getViewport(view);
+            gl.viewport(viewport.x, viewport.y,
+                        viewport.width, viewport.height);
+            if (view.camera &&  captureNext) {
+				captureNext =false
+				dcamera = view.camera
+				camBinding = glBinding.getCameraImage(dcamera);
+				texture1 = drawCameraCaptureScene(gl, camBinding,  dcamera.width, dcamera.height)
+				
+				mesh.quaternion.copy(camera.quaternion)
+				mesh.position.copy(camera.position)
+				mesh.rotateZ(3*Math.PI/2)
+				updateServer(texture1)
+            } else {
+              console.log('unavailable')
+			}
+        }
+	}
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+init()
+render()
+function render() {
+	renderer.render( scene, camera );
+}
+
+var button = document.createElement( 'button' );
+button.id = 'ArButton'
+button.textContent = 'ENTER AR' ;
+button.style.cssText+= `position: absolute;top:80%;left:40%;width:20%;height:2rem;`;
+    
+document.body.appendChild(button)
+document.getElementById('ArButton').addEventListener('click',x=>AR())
