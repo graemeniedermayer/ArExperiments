89e0141f83fb8b0cdbfd6277807f10ab4bfa13e7
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-07-25 21:48:46 -0600

    Update routing.py

--- python/django/websocket/routing.py
+++ python/django/websocket/routing.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import consumers
 
-# This contain regex (regular expressions.) ENSURE THIS IS SECURE AGAINST ATTACKS!!! regex is an opening. Oh shit...
+# This contain regex (regular expressions.) Ensure this is secure against regex timeout attacks
 websocket_urlpatterns = [
     url(r'^ws/arMobile/(?P<channelName>[^/]+)$', consumers.mobileDevice.as_asgi()),
     url(r'^ws/arDesktop/(?P<channelName>[^/]+)$', consumers.desktopDevice.as_asgi())
