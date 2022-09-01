d3afb5a8ed5f8c21052fc57940aeb4fa3e5cd958
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-08-31 21:06:37 -0600

    Create catanTerrain.js

---
+++ javascript/catanTerrain.js
@@ -0,0 +1,23 @@
+
+let biomes = [
+    'forest',
+    'sheep',
+    'praire',
+    'mountain',
+    'desert'
+]
+
+let biomeParameters = [
+    forest: {height: , perlinNoise: , perlinAd: , perlinWidth:},
+    sheep: {height: , perlinNoise: , perlinAd: , perlinWidth:},
+    praire: {height: , perlinNoise: , perlinAd: , perlinWidth:},
+    mountain: {height: , perlinNoise: , perlinAd: , perlinWidth:},
+    desert: {height: , perlinNoise: , perlinAd: , perlinWidth:}
+]
+
+// create a surface
+
+let terrain = 
+
+let water = 
+
